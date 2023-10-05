4d8732dea52757dda4cfc66543ee6a6bab05a371
romchyk1219 <romchyk1219@icloud.com>
2023-10-05 18:01:27 -0400

    Added Rubies (no use yet)

--- src/main/java/net/romchyk1219/redupdate/item/ModItems.java
+++ src/main/java/net/romchyk1219/redupdate/item/ModItems.java
@@ -1,13 +1,21 @@
 package net.romchyk1219.redupdate.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.romchyk1219.redupdate.RedUpdate;
 
 public class ModItems {
+    public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
 
+    private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RUBY);
+    }
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(RedUpdate.MOD_ID, name), item);
@@ -15,5 +23,6 @@ public class ModItems {
 
     public static void registerModItems() {
         RedUpdate.LOGGER.info("Registering Mod Items for " + RedUpdate.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientTabItemGroup);
     }
 }
